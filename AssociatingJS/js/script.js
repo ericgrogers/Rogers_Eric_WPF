b46cf77c9e133a37329e006be65ae2269ed2c12a
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-01 13:42:26 -0600

    Screencast: Assignment Operators

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,5 @@
-// Casting
+// Expressions - Assignment Operators
 
-var areaCode = 407;
-var fistPart = 203;
-var secPart = 4529;
-
-var phoneNo = "(" + String(areaCode) + ")" + " " +  String(fistPart) + "-" + String(secPart);
-console.log(phoneNo);
+var a = 3;
+a += 4; // a = a + 4
+console.log(a);
