d8c247d224420251dcf6aaf9a5d328e57b1afbd2
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-01 14:31:01 -0600

    Added the Dog Years solution

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,3 +2,17 @@
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
+
+
+
+// Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+var sparkyAge = 7;
+
+// Multiply sparkyAge by 7 to determine his age in dogYears
+
+var dogYears = sparkyAge * 7;
+
+// Print out sparky's human age, as well as his age in dog years.
+
+console.log("Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years.")
