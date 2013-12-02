bfbc571e8346725ab96cd4e9f69b8e5ef6f247f4
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-01 20:49:01 -0600

    Set up file structure

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,13 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+
+
+// Eric Rogers
+// 12/01/13
+// Expressions
+
+
+
