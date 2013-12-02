8b04ebafa439a1bd9fe6bf83be65e82fc3fc2510
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-01 21:00:52 -0600

    Added variable to record how many days per week user drinks coffee

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -14,6 +14,10 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // ask the user for the price of a cup of coffee and save it to pricePerCup
 var pricePerCup = prompt("How much do you pay for a cup of Coffee?");
 
+// ask the user how many days per week coffee is drank and save it to daysPerWeek
+var daysPerWeek = prompt("How many days per week do you drink Coffee?");
+
+
 console.log(pricePerCup);
 
 
