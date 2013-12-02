14561e42fc0d001f4b72dad5c3e40dcf6f784f6d
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-01 21:00:06 -0600

    Added variable to grab the price per cup from the user.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,7 +7,14 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 // Eric Rogers
 // 12/01/13
-// Expressions
+// Expressions: Personal
+
+// How much do I spend on coffee per year.
+
+// ask the user for the price of a cup of coffee and save it to pricePerCup
+var pricePerCup = prompt("How much do you pay for a cup of Coffee?");
+
+console.log(pricePerCup);
 
 
 
