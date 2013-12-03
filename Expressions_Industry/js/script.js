fd8b2e4eaff731396d6cc3565a825e7e92964f46
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-02 20:21:06 -0600

    Added program description

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,3 +7,7 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // 12/02/13
 // Expressions: Industry-Specific
 
+/* This program can be used to fill out a simple weekly paycheck and paystub. It will print out the company name of the check issuer, the person that is receiving the check, and the amount being paid on the check portion. The paystub portion will show the company's name, the employee's name, the number of hours of normal pay, the number of hours of overtime pay, the amount paid for normal hours, the amount paid for overtime hours, and a total pay. This program does not include deductions or taxes. The program will allow you to input hours and overtime hours on a day by day basis for a 5 day work week. It will also show a running total through day 5. */
+
+
+
