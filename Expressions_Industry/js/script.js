0935f532b00e8a8dbe08a14b54fbfc061ec350da
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-02 22:16:45 -0600

    Added the date and total hours to the description. Added prompt for date.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,13 +7,16 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // 12/02/13
 // Expressions: Industry-Specific
 
-/* This program can be used to fill out a simple weekly paycheck and paystub. It will print out the company name of the check issuer, the employee that is receiving the check, and the amount being paid on the check portion. The paystub portion will show the company's name, the employee's name, the number of hours of normal pay, the number of hours of overtime pay, the amount paid for normal hours, the amount paid for overtime hours, and a total pay. This program does not include deductions or taxes. The program will allow you to input hours and overtime hours on a day by day basis for a 5 day work week. It will also show a running total of hours to the user through day 5. */
+/* This program can be used to fill out a simple weekly paycheck and paystub. It will print out the company name of the check issuer, the employee that is receiving the check, the date, and the amount being paid on the check portion. The paystub portion will show the company's name, the employee's name, the date, the number of hours of normal pay, the number of hours of overtime pay, the amount paid for normal hours, the amount paid for overtime hours, total hours, and a total pay. This program does not include deductions or taxes. The program will allow you to input hours and overtime hours on a day by day basis for a 5 day work week. It will also show a running total of hours to the user through day 5. */
 
 // creating a variable to store the total amount of normal hours worked. Setting the starting total to 0.
 var totalNormalHours = 0;
 
 // creating a variable to store the total amount of overtime hours worked. Setting the starting total to 0.
 var totalOvertimeHours = 0;
+
+// prompting the user for the date and saving it to the date variable.
+var date = prompt("Please enter the date in the following format: \nMM/DD/YYYY");
 
 // getting the company's name from the user by using a prompt
 var companyName = prompt("Please enter the Company name to be printed on the check.");
@@ -28,13 +31,14 @@ var hourlyRate = prompt("Please enter the normal hourly rate paid to the employe
 var overtimeRate = prompt("Please enter the overtime rate paid to the employee.");
 
 // prompt the user to enter the amount of normal hours worked on Monday and save it to normalHoursMon
-var normalHoursMon = prompt("Please enter the number of Normal Hours worked on Monday.");
-
-// prompt the user to enter the amount of overtime hours worked on Monday and save it to overtimeHoursMon
-var overtimeHoursMon = prompt("Please enter the number of Overtime Hours worked on Monday.");
+var normalHoursMon = prompt("Total Normal Hours: " + totalNormalHours + "\nTotal Overtime Hours: " + totalOvertimeHours + "\nPlease enter the number of Normal Hours worked on Monday.");
 
 // adjusting the total of normal hours by adding monday's hours to the previous total.
 totalNormalHours += Number(normalHoursMon);
+
+// prompt the user to enter the amount of overtime hours worked on Monday and save it to overtimeHoursMon
+var overtimeHoursMon = prompt("Total Normal Hours: " + totalNormalHours + "\nTotal Overtime Hours: " + totalOvertimeHours + "\nPlease enter the number of Overtime Hours worked on Monday.");
+
 
 // adjusting the total of overtime hours by adding monday's hours to the previous total.
 totalOvertimeHours += Number(overtimeHoursMon);
@@ -96,9 +100,18 @@ var overtimePayTotal = Number(totalOvertimeHours) * Number(overtimeRate);
 // add normalPayTotal to overtimePayTotal to get the totalPay.
 var totalPay = Number(normalPayTotal) + Number(overtimePayTotal);
 
-// creating the check by outputting the company name, the employee's name, and total pay with a console message.
-var check = "Check to be printed: \n \nCompany Name: " + companyName + "\n \nPay to the order of: " + employee + ", Amount: $" + totalPay;
+// add totalNormalHours to totalOvertimeHours to create the totalHours variable.
+var totalHours = Number(totalNormalHours) + Number(totalOvertimeHours);
 
+// creating the check by saving the company's name, the employee's name, and total pay to the check variable.
+var check = "Check to be printed: \n \nCompany Name: " + companyName + "     Date: " + date + "\n \nPay to the order of: " + employee + ", Amount: $" + totalPay;
 
-console.log(check);
+// creating the check stub by saving the company's name, the employees name, the total normal hours worked, the total overtime hours worked, the total normal pay, the total overtime pay, and the total pay to the checkStub variable.
+var checkStub = "\n \nCheck Stub to be printed: \n \nCompany Name: " + companyName + "\nEmployee Name: " + employee + "Date:     " + date + "\n \nNormal Hours: " + totalNormalHours + "     Normal Wages: $" + normalPayTotal + "\n \nOvertime Hours: " + totalOvertimeHours + "     Overtime Wages: $" + overtimePayTotal + "\n \nTotal Hours: " + totalHours + "     Total Wages: $" + totalPay;
+
+// creating a result variable to hold the check and checkStub.
+var result = check + checkStub;
+
+// outputting the result to the console.
+console.log(result);
 
