89fad87a14b9d54ab92484444f41a8e738324126
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-02 20:03:35 -0600

    Added file structure

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,9 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+// Eric Rogers
+// 12/02/13
+// Expressions: Industry-Specific
+
