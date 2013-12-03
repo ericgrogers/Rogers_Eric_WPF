f8e5be266108397ec4b1a5045faae6b68ead78da
ericgrogers <ericrogers@Erics-MacBook-Pro.local>
2013-12-02 20:51:51 -0600

    added variables for total normal hours, and total overtime hours. Added prompt for Tuesday's hours w/total hours

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,6 +9,12 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 /* This program can be used to fill out a simple weekly paycheck and paystub. It will print out the company name of the check issuer, the employee that is receiving the check, and the amount being paid on the check portion. The paystub portion will show the company's name, the employee's name, the number of hours of normal pay, the number of hours of overtime pay, the amount paid for normal hours, the amount paid for overtime hours, and a total pay. This program does not include deductions or taxes. The program will allow you to input hours and overtime hours on a day by day basis for a 5 day work week. It will also show a running total through day 5. */
 
+// creating a variable to store the total amount of normal hours worked. Setting the starting total to 0.
+var totalNormalHours = 0;
+
+// creating a variable to store the total amount of overtime hours worked. Setting the starting total to 0.
+var totalOvertimeHours = 0;
+
 // getting the company's name from the user by using a prompt
 var companyName = prompt("Please enter the Company name to be printed on the check.");
 
@@ -25,6 +31,13 @@ var overtimeRate = prompt("Please enter the overtime rate paid to the employee."
 var normalHoursMon = prompt("Please enter the number of Normal Hours worked on Monday.");
 
 // prompt the user to enter the amount of overtime hours worked on Monday and save it to overtimeHoursMon
-var overtimeHoursMon = prompt("Please enter the number of Overtime Hours worked on Monday.")
+var overtimeHoursMon = prompt("Please enter the number of Overtime Hours worked on Monday.");
 
+// adjusting the total of normal hours by adding monday's hours to the previous total.
+totalNormalHours += Number(normalHoursMon);
 
+// adjusting the total of overtime hours by adding monday's hours to the previous total.
+totalOvertimeHours += Number(overtimeHoursMon);
+
+//prompt the user to enter the amount of normal hours worked on Tuesday and save it to normalHoursTue. Include the running total of normal and overtime hours in the prompt.
+var normalHoursTue = prompt("Total Normal Hours: " + totalNormalHours + "\nTotal Overtime Hours: " + totalOvertimeHours + "\nPlease enter the number of Normal Hours worked on Tuesday.");
