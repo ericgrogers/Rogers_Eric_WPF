84047012ad1cb0460f6e5047a94f9bfc65f2b77e
ericgrogers <ericgrogers@fullsail.edu>
2013-12-04 19:34:54 -0600

    set up the three prompts

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -15,3 +15,13 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 var muppetCount = [20];
 var rooms;
 var miles;
+var guess;
+
+// prompting the user to guess the total muppets, then storing the guess in the variable guess.
+guess = prompt("Guess how many Muppets it would take to move your house! (numbers only please)");
+
+// asking user how many rooms their house has using a prompt, then storing their answer in the variable rooms.
+rooms = prompt("How many rooms does your house have?");
+
+// asking user how far they will be moving their house, in miles, then storing their answer in the variable miles.
+miles = prompt("How many miles will the Muppets be moving your house?").
