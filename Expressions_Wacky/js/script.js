898c7e26521f68edf2cc6c3010d76b9e43f5dad0
ericgrogers <ericgrogers@fullsail.edu>
2013-12-04 19:23:28 -0600

    Added file structure.

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,9 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+// Eric Rogers
+// Expressions: Wacky
+// 12/04/2013
+
