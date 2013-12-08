ee316d4aebe6272690cf400a41ae158fd358db78
ericgrogers <ericgrogers@fullsail.edu>
2013-12-07 20:09:07 -0600

    declaring the givens as variables

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -7,3 +7,14 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // 12/07/2013
 // Conditionals Worksheet
 
+// Last Chance for Gas!
+
+/* A driver has to determine if they can make it across the desert with their current fuel.
+ They are about to past the last gas station for the next 200 miles and they need to determine
+ whether they should stop now for gas or not. */
+
+// declaring the givens as variables
+var gasEfficiency ;
+var gasGauge ;
+var tankCapacity ;
+
