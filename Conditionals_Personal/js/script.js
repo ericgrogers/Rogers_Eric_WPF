6d739b5025e20336d6a6b40dd82e607589bf8b1b
ericgrogers <ericgrogers@fullsail.edu>
2013-12-08 18:46:20 -0600

    setting up the file structure. added a program description

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,12 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+
+// Eric Rogers
+// 12/08/2013
+// Conditionals: Personal
+
+/* This program determines how long a person would have to save for a vacation. It factors your bills, your income, and the price of the vacation to determine how many months it will take to get enough money together.
+
