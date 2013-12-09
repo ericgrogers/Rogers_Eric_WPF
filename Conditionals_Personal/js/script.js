cc15a6df8402f72fe422e8b521a15f5ac0b0d1ad
ericgrogers <ericgrogers@fullsail.edu>
2013-12-08 19:15:55 -0600

    declared all necessary variables

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,5 +8,19 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // 12/08/2013
 // Conditionals: Personal
 
-/* This program determines how long a person would have to save for a vacation. It factors your bills, your income, and the price of the vacation to determine how many months it will take to get enough money together.
+/* This program determines how long a person would have to save for a vacation. It factors your bills, your income, and the price of the vacation to determine how many months it will take to get enough money together. */
+
+// declaring all of the necessary variables.
+var monthlySalary;
+var monthlySpending;
+var monthlyExtraMoney;
+var electric;
+var water;
+var home;
+var phone;
+var insurance;
+var vacationPrice;
+var spendingMoney;
+var monthsToSave;
+var results;
 
