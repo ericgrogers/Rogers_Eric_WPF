ac9c24748cf29a9709c349e060d48c72615abe67
ericgrogers <ericgrogers@fullsail.edu>
2013-12-09 22:13:48 -0600

    added all necessary variables

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -8,5 +8,21 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // 12/09/2013
 // Conditionals: Industry
 
-/* This program is a basic billing and payment calculator for the web design/development industry. It will calculate the amount to be billed for 3 different services at different hourly rates, and include an optional maintenance package if the client desires one. It will figure out the retainer fee to be charged, as well as the number of payments and amounts to be paid before final delivery.
+/* This program is a basic billing and payment calculator for the web design/development industry. It will calculate the amount to be billed for 3 different services at different hourly rates, and include an optional maintenance package if the client desires one. It will figure out the retainer fee to be charged, as well as the number of payments and amounts to be paid before final delivery. */
 
+// declaring necessary variables.
+var reply;
+var visualDesigns = [50, 0]; // Pay per hour, hours to complete
+var htmlCssTemplates = [40, 0]; // Pay per hour, hours to complete
+var programming = [60, 0]; // Pay per hour, hours to complete
+var maintenance1 = [1200, 16, 1]; // Price, 16 hours of service, over 1 month
+var maintenance2 = [3000, 24, 3]; // Price, 24 hours of service, over 3 months
+var maintenance3 = [5000, 40, 6]; // Price, 40 hours of service, over 6 months
+var maintenanceChoice = 0; // package selection
+var totalHours; // total working hours
+var standardBill; // total of standar services
+var totalBill = 0; // total bill with or without maintenance
+var retainer; // retainer fee
+var onePayment; // for one additional payment
+var twoPayments; // for two additional payments
+var results; // final results
