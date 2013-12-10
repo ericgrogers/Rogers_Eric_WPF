6aacd2339f459772dd56c4ffda16428e6ae24df9
ericgrogers <ericgrogers@fullsail.edu>
2013-12-09 21:39:52 -0600

    setup file structure and added program description.

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,12 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+
+// Eric Rogers
+// 12/09/2013
+// Conditionals: Industry
+
+/* This program is a basic billing and payment calculator for the web design/development industry. It will calculate the amount to be billed for 3 different services at different hourly rates, and include an optional maintenance package if the client desires one. It will figure out the retainer fee to be charged, as well as the number of payments and amounts to be paid before final delivery.
+
