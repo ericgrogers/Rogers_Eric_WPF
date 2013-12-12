f476351ce0bbcf9e7dce0b18c8b8fcd3fba8521a
ericgrogers <ericgrogers@fullsail.edu>
2013-12-11 20:35:39 -0600

    added program description

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,3 +8,5 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // 12/11/2013
 // Conditionals: Wacky
 
+/* For this program, the user is going to have a snowball fight with Frosty the Snowman. The program asks the user for their name, and then asks if they would like to see the rules for the game. When the game starts, the computer generates a random number between 1 and 5 and then asks the user to guess at the number. If the user get it right, it records a hit, otherwise, the user misses and then the computer takes a turn. The computer will make a random guess and then it goes back to the user. Both the user and Frosty will guess 3 times, and then the hits will be totaled. Then it will declare a winner, or a tie will occur.*/
+
