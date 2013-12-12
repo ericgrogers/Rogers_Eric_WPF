45941a0347d4f4c30bb76414861f974e49766d69
ericgrogers <ericgrogers@fullsail.edu>
2013-12-11 20:09:24 -0600

    Setup file structure.

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,10 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+
+// Eric Rogers
+// 12/11/2013
+// Conditionals: Wacky
+
