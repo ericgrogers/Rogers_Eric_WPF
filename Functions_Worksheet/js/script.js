aa2bd1d33c32a46f294de5ca94404a3cae50b255
ericgrogers <ericgrogers@fullsail.edu>
2013-12-14 13:18:42 -0600

    added personal info and problem 1 description.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -2,3 +2,11 @@
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
+
+// Eric Rogers
+// 12/14/2013
+// Functions Worksheet
+
+// Circumference
+// calculate the circumference of a circle using the radius.  ( c=2πr)
+
