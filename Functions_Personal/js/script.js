6df3bb5e2f74fb460f9e8555234dc37ea2ee5531
ericgrogers <ericgrogers@fullsail.edu>
2013-12-19 17:56:46 -0600

    Setup file structure.

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,10 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+
+// Eric Rogers
+// 12/18/13
+// Funcitons: Personal
+
