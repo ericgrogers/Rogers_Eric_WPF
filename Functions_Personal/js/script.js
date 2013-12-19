48b6d80e95a00c9de290af409edf98c622a5c028
ericgrogers <ericgrogers@fullsail.edu>
2013-12-19 17:59:42 -0600

    added program description.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -8,3 +8,5 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // 12/18/13
 // Funcitons: Personal
 
+// This program is used to create a playlist of mp3's and sort them alphabetically.
+
