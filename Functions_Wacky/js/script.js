aff45e6431de1fed020f48a80ba558b5d0f4e201
ericgrogers <ericgrogers@fullsail.edu>
2013-12-19 20:07:25 -0600

    Added variables.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -9,3 +9,10 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 // How long will Santa Clause need to run on a treadmill to burn off all of the calories from the cookies that the children left for him on Christmas eve?
 
+// add all necessary variables.
+
+var cookieCalories = 59; // set the cookieCalories to 59.
+var treadmillSpeed = 2; // set the treadmillSpeed to 2mph.
+var caloriesPerMile = 100; // set the caloriesPerMile to 100.
+var hoursInDay = 24; // set the hoursInDay to 24.
+var results = "Santa will have to run on the treadmill for " + main() + " to burn off all those calories from the children's cookies!";  // set the results to be a brief message and a function call to main.
