84ab5eddf2b27605397d873f44bb07cb0aee6802
ericgrogers <ericgrogers@fullsail.edu>
2013-12-19 20:04:31 -0600

    Added program description.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,3 +7,5 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 // 12/18/13
 // Functions: Wacky
 
+// How long will Santa Clause need to run on a treadmill to burn off all of the calories from the cookies that the children left for him on Christmas eve?
+
