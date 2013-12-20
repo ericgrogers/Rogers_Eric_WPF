9031b1891cd94ceb4cf628c5c84f889f620b3f92
ericgrogers <ericgrogers@fullsail.edu>
2013-12-19 18:10:01 -0600

    added getName function for the playlist name.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -24,3 +24,13 @@ function main(){
     playlistName = getName(); // call the getName function and save the results to playlistName.
     addMp3(); // call the addMp3 function.
 }
+
+// create a function to get the playlist name.
+
+function getName(){
+    var plName = prompt("Please enter a name for your playlist"); // prompt the user for a playlist name and store it to plName.
+    while(plName === ""){ // while plName is empty.
+        plName = prompt("You must enter a name. Please enter a name for your playlist."); // continue to prompt the user for a playlist name.
+    }
+    return plName;  // return the playlist name
+}
