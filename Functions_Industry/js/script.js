acf38f9b98bfc76c33e526ee4c06855f8864f4ab
ericgrogers <ericgrogers@fullsail.edu>
2013-12-19 19:03:44 -0600

    Setup file structure

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,12 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+// Eric Rogers
+// 12/18/13
+// Functions: Industry
+
+/* This program will calculate the total height of a photo gallery web page, as well as the size of the photos. The user can input the page width, the header height, the footer height, the number of photos to be used, and how many photos per row to display. It will automatically add a 30 pixel padding to the page, and 30 pixels of margin between all elements to keep everything properly aligned. */
+
+ 
