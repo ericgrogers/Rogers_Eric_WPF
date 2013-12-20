4f84df17cfb731efadc74e54dff3380fa2e3d09b
ericgrogers <ericgrogers@fullsail.edu>
2013-12-19 18:04:22 -0600

    created necessary variables.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -10,3 +10,10 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 // This program is used to create a playlist of mp3's and sort them alphabetically.
 
+
+// add the necessary variables
+
+
+var mp3s = []; // set an array to store the list of mp3's
+var results; // set a variable to hold the final results.
+var playlistName; // set a variable to hold the playlist name.
