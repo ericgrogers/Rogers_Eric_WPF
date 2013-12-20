1658bb69685fa18e0688be18292e681e198431e6
ericgrogers <ericgrogers@fullsail.edu>
2013-12-19 19:05:45 -0600

    Added necessary variables.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,4 +9,7 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 /* This program will calculate the total height of a photo gallery web page, as well as the size of the photos. The user can input the page width, the header height, the footer height, the number of photos to be used, and how many photos per row to display. It will automatically add a 30 pixel padding to the page, and 30 pixels of margin between all elements to keep everything properly aligned. */
 
- 
+// add the necessary variables.
+
+var marginSize = 30; // set the marginSize to 30.
+var gutterSize = 30; // set the gutterSize to 30.
